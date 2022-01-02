988ebea21d2a929ecddc6c38ef3ccc3b9d5857ad
Garipov-Amir <62028150+Garipov-Amir@users.noreply.github.com>
2022-01-02 17:49:39 +0300

    fefefe
    jheoifejhoif

---
+++ include/view.php
@@ -0,0 +1,15 @@
+<?php
+	if (SUPRA !== 1) die();
+
+	/*
+	 * @autor: Bday
+	 * @version: 1.0.0
+	 *
+	 * Class View
+	 */
+	class View {
+
+		public function __construct() {
+			new Html();
+		}
+	}
